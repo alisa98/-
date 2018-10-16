fafce9f9094bfbc25d052e5d9119ea7451062413
ALisa Utlik <alisa.utlik@gmail.com>
2018-10-16 12:00:12 +0300

    add quick sort and unit test

---
+++ QuickSort/Sorting/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Sorting
+    {
+       
+
+        static void Swap(int[] A, int i, int j)
+        {
+            var temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+
+       
+        static int Partition(int[] A, int p, int r)
+        {
+            var x = A[p];
+            int i = p;
+            for (int j = p + 1; j < r + 1; j++)
+            {
+                if (A[j] <= x)
+                {
+                    i++;
+                    Swap(A, i, j);
+                }
+            }
+            Swap(A, p, i);
+            return i;
+        }
+
+        static void QuickSort(int[] A, int p, int r)
+        {
+            if (p < r)
+            {
+                int q = Partition(A, p, r);
+                QuickSort(A, p, q - 1);
+                QuickSort(A, q + 1, r);
+            }
+        }
+    }
+}
---
+++ QuickSort/Test/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+       
+       
+
+ [TestMethod]
+        public void Quicksort_RegularIntArrayPassed_ArrayIsSorted()
+        {
+            int[] source = new[] { 4, 5, 9, 1, 2, 6, 3, 7, 8 };
+            var p = 2;
+
+            var r = 5;
+
+            int[] expected = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Sorting.Quicksort(source, p, r);
+            Assert.AreEqual(expected, source);
+        }
+
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
